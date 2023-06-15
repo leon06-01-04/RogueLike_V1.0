918bc6604ee9cd8af6c1ea7a16d9232c6c315bd8
r0seA <134058374+r0seA@users.noreply.github.com>
2023-06-15 09:51:23 +0200

    Update HandlerCreature.java

--- src/main/HandlerCreature.java
+++ src/main/HandlerCreature.java
@@ -8,15 +8,16 @@ public class HandlerCreature {
     public LinkedList<Entity> object = new LinkedList<Entity>();
 
     public void tick() {
-
+      //updaten des objects
         for (Entity tempObjekt : object) {
             tempObjekt.tick();
+            
         }
 
     }
 
     public void render(Graphics g) {
-
+           //das rendern von Grafiken
         for (Entity tempObject : object) {
             tempObject.render(g);
         }
